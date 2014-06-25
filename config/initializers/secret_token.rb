cddf17f84f4ec1eb4504eaad9bcaeaf6145de531
Nico <nicogaldamez@gmail.com>
2014-06-25 19:05:26 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RenoApp::Application.config.secret_key_base = 'c37e34f02be5cdfbabbc842e7ce03b54989b006e1cfc21d84bb4db07249b667ca60740bef07916f79081ac40f06539ef5100c59cf9dd6841cc6aea2e5dc72b9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RenoApp::Application.config.session_store :cookie_store, key: '_reno_app_session'
