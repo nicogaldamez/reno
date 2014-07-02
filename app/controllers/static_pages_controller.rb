2ffcdfc8c67f506a27a4269a4d5db69d3986c47a
Nico <nicogaldamez@gmail.com>
2014-07-02 16:01:48 -0300

    Ajustes visuales

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def index
+    @menu = 'inicio'
   end
   
   def compromiso
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def nav_link(link_text, link_path)
+    # logger.debug current_page?()
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+  
 end
