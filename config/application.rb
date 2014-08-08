419d3aa95a23cf7d1a1f4662931d7d4757028b31
Nico <nicogaldamez@gmail.com>
2014-08-08 16:51:39 -0300

    Assets

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,6 @@ module RenoApp
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
+    config.assets.initialize_on_precompile = true
   end
 end
